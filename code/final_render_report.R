d604d68ae851934944c1071adf9472b5a689bdc3
Susan Choi <kaeum11@gmail.com>
2025-11-29 15:57:37 -0500

    update render code

--- code/final_render_report.R
+++ code/final_render_report.R
@@ -2,6 +2,12 @@ here::i_am(
   "code/final_render_report.R"
 )
 
+# generate table
+source("code/final_make_table1.R")
+
+# generate figure
+source("code/final_make_figures.R")
+
 rmarkdown::render(
   here::here("Final-Project.Rmd")
 )
